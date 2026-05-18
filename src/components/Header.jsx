972b4f6966a97d2f1b3114a76ed612d94756b1a0
M. Arif Mardhavi <arifmardhavi@gmail.com>
2026-05-18 17:16:29 +0900

    asd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({ dark, setDark }) {
             }}
           className="px-3 py-1 rounded bg-gray-200 dark:bg-gray-700 cursor-pointer"
         >
-          🌙
+            {dark ? 'Light Mode' : 'Dark Mode'}
         </button>
 
         <button
